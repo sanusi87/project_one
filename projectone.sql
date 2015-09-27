e830aea6487b8525df8bba3733d76cade859f9e1
Mohd Sanusi <sanusi87@gmail.com>
2015-09-27 13:26:30 +0800

    add gender, race, date of birth column

--- projectone.sql
+++ projectone.sql
@@ -1,19 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.1.3.1
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Sep 27, 2015 at 01:45 AM
--- Server version: 5.1.33
--- PHP Version: 5.2.9
+-- Host: 127.0.0.1
+-- Generation Time: Sep 27, 2015 at 07:25 AM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `projectone`
@@ -26,12 +27,11 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `full_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+  `full_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `admin`
@@ -47,22 +47,24 @@ INSERT INTO `admin` (`id`, `username`, `password`, `full_name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `application` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `student_id` int(10) unsigned NOT NULL,
   `school_id` int(5) unsigned NOT NULL,
   `subject_id` int(5) unsigned NOT NULL,
   `status` tinyint(3) unsigned NOT NULL,
   `date_added` datetime NOT NULL,
-  `date_updated` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+  `date_updated` datetime DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `application`
 --
 
 INSERT INTO `application` (`id`, `student_id`, `school_id`, `subject_id`, `status`, `date_added`, `date_updated`) VALUES
-(1, 4, 1, 1, 2, '2015-07-18 11:43:30', '0000-00-00 00:00:00');
+(1, 4, 2, 2, 2, '2015-07-18 11:43:30', '2015-09-27 07:20:39'),
+(2, 4, 2, 2, 3, '2015-09-27 07:05:46', '2015-09-27 07:22:11'),
+(3, 4, 2, 2, 3, '2015-09-27 07:06:53', '2015-09-27 07:22:23'),
+(4, 4, 2, 2, 1, '2015-09-27 07:18:57', '2015-09-27 07:19:24');
 
 -- --------------------------------------------------------
 
@@ -71,11 +73,10 @@ INSERT INTO `application` (`id`, `student_id`, `school_id`, `subject_id`, `statu
 --
 
 CREATE TABLE IF NOT EXISTS `application_status` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `application_status`
@@ -93,10 +94,9 @@ INSERT INTO `application_status` (`id`, `name`, `description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `city` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `id` int(10) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `city`
@@ -113,11 +113,10 @@ INSERT INTO `city` (`id`, `name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `faculty` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `long_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `short_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `short_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `faculty`
@@ -134,11 +133,10 @@ INSERT INTO `faculty` (`id`, `long_name`, `short_name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `programme` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `long_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `short_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `short_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `programme`
@@ -155,12 +153,11 @@ INSERT INTO `programme` (`id`, `long_name`, `short_name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `school` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `state_id` int(10) unsigned NOT NULL,
-  `city_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `city_id` int(10) unsigned NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `school`
@@ -196,10 +193,9 @@ INSERT INTO `school_subject` (`school_id`, `subject_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `state` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `id` int(10) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `state`
@@ -216,24 +212,29 @@ INSERT INTO `state` (`id`, `name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `student` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `matric_no` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `programme` tinyint(4) unsigned NOT NULL,
   `faculty` tinyint(4) unsigned NOT NULL,
   `full_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `date_created` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+  `gender` enum('F','M') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'M',
+  `dob` datetime NOT NULL,
+  `race` tinyint(3) unsigned NOT NULL,
+  `date_created` datetime NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`id`, `matric_no`, `password`, `programme`, `faculty`, `full_name`, `date_created`) VALUES
-(5, 'd20092036960', '827ccb0eea8a706c4c34a16891f84e7b', 1, 1, NULL, '2015-08-03 03:53:50'),
-(3, '123', '202cb962ac59075b964b07152d234b70', 2, 2, NULL, '2015-07-18 01:21:10'),
-(4, '456', '250cf8b51c773f3f8dc8b4be867a9a02', 2, 2, NULL, '2015-07-18 02:55:48');
+INSERT INTO `student` (`id`, `matric_no`, `password`, `programme`, `faculty`, `full_name`, `gender`, `dob`, `race`, `date_created`) VALUES
+(5, 'd20092036960', '827ccb0eea8a706c4c34a16891f84e7b', 1, 1, 'y', 'M', '0000-00-00 00:00:00', 0, '2015-08-03 03:53:50'),
+(3, '123', '202cb962ac59075b964b07152d234b70', 2, 2, 'x', 'M', '0000-00-00 00:00:00', 0, '2015-07-18 01:21:10'),
+(4, '456', '250cf8b51c773f3f8dc8b4be867a9a02', 2, 2, 'z', 'M', '0000-00-00 00:00:00', 0, '2015-07-18 02:55:48'),
+(6, '1234', '81dc9bdb52d04dc20036dbd8313ed055', 1, 0, NULL, 'M', '0000-00-00 00:00:00', 0, '2015-09-27 04:41:17'),
+(7, '999', 'b706835de79a2b4e80506f582af3676a', 2, 0, NULL, 'M', '2015-09-28 00:00:00', 1, '2015-09-27 05:32:02'),
+(8, 'xxx', 'f561aaf6ef0bf14d4208bb46a4ccb3ad', 1, 0, 'xxx', 'F', '2015-09-23 00:00:00', 2, '2015-09-27 07:23:02');
 
 -- --------------------------------------------------------
 
@@ -242,10 +243,9 @@ INSERT INTO `student` (`id`, `matric_no`, `password`, `programme`, `faculty`, `f
 --
 
 CREATE TABLE IF NOT EXISTS `subject` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `id` int(10) unsigned NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `subject`
@@ -254,3 +254,125 @@ CREATE TABLE IF NOT EXISTS `subject` (
 INSERT INTO `subject` (`id`, `name`) VALUES
 (1, 'Subject One'),
 (2, 'Subject Two');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `application`
+--
+ALTER TABLE `application`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `application_status`
+--
+ALTER TABLE `application_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `programme`
+--
+ALTER TABLE `programme`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `school`
+--
+ALTER TABLE `school`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `application`
+--
+ALTER TABLE `application`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `application_status`
+--
+ALTER TABLE `application_status`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `programme`
+--
+ALTER TABLE `programme`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `school`
+--
+ALTER TABLE `school`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
