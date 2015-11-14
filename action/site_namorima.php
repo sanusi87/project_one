8d645df243661e059a26205aa81d6847c77f96ef
Mohd Sanusi <sanusi87@gmail.com>
2015-11-14 11:11:24 +0800

    contoh content

--- action/site_namorima.php
+++ action/site_namorima.php
@@ -1,1 +1,16 @@
-buh gapo
+<?php
+// boh goni dulu
+ob_start();
+
+// lp0s tu ikut mu nk boh gapo dale ni
+?>
+
+<h1>Panduan Pelajar</h1>
+<p>Panduan Pelajar boleh di dapat dalam fail PDF di pautan di bawah</p>
+<p><a href="../error_log.txt">PDF</a></p>
+
+<?php
+// last skali boh goni wojib
+$content = ob_get_clean();
+View::addContent( $content );
+?>
