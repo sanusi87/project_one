e49dbadfcfc7b0d00c78ca836472f1728d71c253
Mohd Sanusi <sanusi87@gmail.com>
2015-12-07 22:21:25 +0800

    add banner and menu bar

--- action/site_about.php
+++ action/site_about.php
@@ -5,6 +5,16 @@ ob_start();
 // lp0s tu ikut mu nk boh gapo dale ni
 ?>
 
+<div class="text-center">
+	<div id="main-banner">
+		<br />
+		<br />
+		<br />
+		<!--<h1>Sistem Pendaftaran Latihan Mengajar</h1>-->
+	</div>
+	<?php echo Component::menuBar(); ?>
+</div>
+
 <h1>Tentang Kami</h1>
 <p>Sistem Pendaftaran Latihan Mengajar (e-splm) ialah sistem yang dibina bagi memudahkan pelajar yang bakal menjalani latihan mengajar </p>
 <p>khususnya untuk pelajar di bawah Fakulti Seni Komputeran dan Industri Kreatif.</p>
