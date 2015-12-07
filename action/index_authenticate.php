479ab596f10b10cc75edb1b61cc9f4ebc00cac4a
Mohd Sanusi <sanusi87@gmail.com>
2015-12-07 22:22:19 +0800

    remove br under banner

--- action/index_authenticate.php
+++ action/index_authenticate.php
@@ -12,30 +12,9 @@ $bangsa = Student::bangsa();
 			<br />
 			<!--<h1>Sistem Pendaftaran Latihan Mengajar</h1>-->
 		</div>
-		
-		<br />
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-				</div>
-
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li><a href="index.php">Utama</a></li>
-						<li><a href="index.php?module=site&action=about">Tentang Kami</a></li>
-						<li><a href="index.php?module=site&action=guide">Panduan</a></li>
-						<li><a href="index.php?module=site&action=contact">Hubungi Kami</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<?php echo Component::menuBar(); ?>
 	</div>
+		
 	<br />
 	<div class="row">
 		<div class="col-md-9 col-sm-9 col-xs-12">
