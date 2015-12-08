dcfc078d4d3db18c3cb987ef67b3f8f0c11c37cf
Mohd Sanusi <sanusi87@gmail.com>
2015-12-08 20:53:36 +0800

    style.css

--- action/index_authenticate.php
+++ action/index_authenticate.php
@@ -11,8 +11,6 @@ $bangsa = Student::bangsa();
 		</div>
 		<?php echo Component::menuBar(); ?>
 	</div>
-		
-	<br />
 	<div class="row">
 		<div class="col-md-9 col-sm-9 col-xs-12">
 			<h2>Selamat Datang ke <?php echo APPNAME; ?></h2>
